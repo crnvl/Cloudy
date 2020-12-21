bb185b158c61367ce890730a23096edd8526913a
shinixsensei <crezoxplays@gmail.com>
2020-12-21 11:48:00 +0100

    token refresh

--- src/main/java/listeners/Commands.java
+++ src/main/java/listeners/Commands.java
@@ -2,21 +2,14 @@ package listeners;
 
 import audio.GuildMusicManager;
 import audio.TrackScheduler;
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.specification.Playlist;
-import com.wrapper.spotify.requests.data.playlists.GetPlaylistRequest;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.apache.hc.core5.http.ParseException;
 import org.jetbrains.annotations.NotNull;
-import spotify.ClientAccess;
 import utilities.Setup;
 
 import java.awt.*;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Commands extends ListenerAdapter {
@@ -30,7 +23,6 @@ public class Commands extends ListenerAdapter {
             JoinVoice joinVoice;
             switch (args[0]) {
                 case "next":
-                    joinVoice = new JoinVoice();
 
                     trackScheduler = new TrackScheduler(GuildMusicManager.player);
                     trackScheduler.clear();
--- src/main/java/spotify/ClientAccess.java
+++ src/main/java/spotify/ClientAccess.java
@@ -6,23 +6,20 @@ import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import utilities.Setup;
 
+import javax.swing.*;
 import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class ClientAccess {
 
     private static final String clientId = Setup.SPOTIFY_CLIENTID;
     private static final String clientSecret = Setup.SPOTIFY_CLIENTSECRET;
-    public static ClientCredentials clientCredentials;
-
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId(clientId)
             .setClientSecret(clientSecret)
             .build();
     private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
             .build();
+    public static ClientCredentials clientCredentials;
 
     public static void clientCredentials_Sync() throws org.apache.hc.core5.http.ParseException {
         try {
@@ -30,14 +27,12 @@ public class ClientAccess {
 
             spotifyApi.setAccessToken(clientCredentials.getAccessToken());
 
-            Runnable helloRunnable = new Runnable() {
-                public void run() {
-                    spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-                }
-            };
-
-            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-            executor.scheduleAtFixedRate(helloRunnable, 0, 1800, TimeUnit.SECONDS);
+            Timer stonks = new Timer(clientCredentials.getExpiresIn() * 1000, e -> {
+                spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+                System.out.println(clientCredentials.getExpiresIn());
+            }
+            );
+            stonks.start();
 
         } catch (IOException | SpotifyWebApiException e) {
             System.out.println("Error: " + e.getMessage());
